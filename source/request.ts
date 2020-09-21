5b91792fffe87f27ccc57b4f32653a2b9817ca7d
Govind <gvind4@gmail.com>
2020-09-21 14:29:07 +0530

    added request base function

---
+++ source/request.ts
@@ -0,0 +1,33 @@
+import fetch, { RequestInit } from 'node-fetch';
+import { URLSearchParams } from 'url';
+
+const API_URL = 'http://en.wikipedia.org/w/api.php?',
+    // RATE_LIMIT = false,
+    // RATE_LIMIT_MIN_WAIT = undefined,
+    // RATE_LIMIT_LAST_CALL = undefined,
+    USER_AGENT = 'wikipedia (https://github.com/dopecodez/Wikipedia/)';
+
+async function makeRequest (params: any): Promise<any> {
+    try{
+        let search = new URLSearchParams({ params })
+        search.set('format', 'json');
+        if (!params.action)
+            search.set('action', 'query')
+        const options: RequestInit = {
+            headers: {
+                'User-Agent': USER_AGENT
+            }
+        }
+        const response = await fetch(API_URL + params, options);
+        return response;
+    } catch(error){
+        throw error;
+    }
+}
+
+export default makeRequest;
+
+
+
+
+
