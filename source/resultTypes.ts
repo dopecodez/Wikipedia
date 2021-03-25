4ce7d5b0773e0c28af62af21cf6a7c8cfb6bd8b8
Kevin Kee <kevinkee9@gmail.com>
2021-03-25 11:42:07 +0900

    random: add more return types
    Added related, mobile-sections. mobile-sections-lead is another return
    format, but is almost identical to mobile-sections (it has one extra
    field, "geo"). Similar enough that no additional type written for
    mobile-sections-lead.
    
    resolving #4

--- source/resultTypes.ts
+++ source/resultTypes.ts
@@ -114,10 +114,7 @@ export interface mediaResult {
   title: string,
   section_id: number,
   type: string,
-  caption?: {
-    html: string,
-    text: string
-  },
+  caption?: htmlText,
   showInGallery: boolean,
   srcset: Array<srcResult>
 }
@@ -182,4 +179,113 @@ export interface titleItem {
 
 export interface title {
   items: Array<titleItem>
+}
+
+export interface related {
+  pages: Array<relatedItem>
+}
+
+export interface relatedItem {
+  pageid: number,
+  ns: number,
+  index: number,
+  type: string,
+  title: string,
+  displaytitle: string,
+  namespace: {
+    id: number,
+    text: string
+  },
+  wikibase_item: number,
+  titles: {
+    canonical: string,
+    normalized: string,
+    display: string
+  },
+  thumbnail: {
+    source: string,
+    width: number,
+    height: number
+  },
+  originalimage: {
+    source: string,
+    width: number,
+    height: number
+  },
+  lang: string,
+  dir: string,
+  revision: string,
+  tid: string,
+  timestamp: string,
+  description: string,
+  description_source: string,
+  content_urls: {
+    desktop: {
+      page: string,
+      revisions: string,
+      edit: string,
+      talk: string
+    },
+    mobile: {
+      page: string,
+      revisions: string,
+      edit: string,
+      talk: string
+    }
+  },
+  extract: string,
+  extract_html: string,
+  normalizedtitle: string
+}
+
+export interface mobileSections {
+  lead: {
+    ns: number,
+    id: number,
+    revision: string,
+    lastmodified: string,
+    lastmodifier: {
+      user: string,
+      gender: string
+    },
+    displaytitle: string,
+    normalizedtitle: string,
+    wikibase_item: string,
+    description: string,
+    description_source: string,
+    protection: Record<string, unknown>,
+    editable: boolean,
+    languagecount: number,
+    image: {
+      file: string,
+      urls: {
+        320: string,
+        640: string,
+        800: string,
+        1024: string
+      }
+    },
+    issues: Array<htmlText>,
+    geo?: {
+      latitude: string,
+      longitude: string
+    }
+    sections: Array<section>
+  },
+  remaining: {
+    sections: Array<section>
+  }
+}
+
+export interface section {
+  id: number,
+  text: string,
+  toclevel: number,
+  line: string,
+  anchor: string
+}
+
+export interface htmlText {
+  html: string,
+  text: string
 }
